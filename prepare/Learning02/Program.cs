e2076d2d6c7560a52b3c1b97c3cc627496c1f85d
gatlingmcgee <rzman03@aol.com>
2023-09-28 21:47:03 -0500

    week3abstract

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "(Microsoft)";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Field Engineer";
+        job2._company = "(Cisco)";
+        job2._startYear = 2010;
+        job2._endYear = 2022;
+
+        job1.DisplayJobDetails();
+        job2.DisplayJobDetails();
     }
 }
