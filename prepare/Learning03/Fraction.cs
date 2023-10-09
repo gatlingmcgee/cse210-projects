f0d179d7731dcf63505cc6e81b03c3c9cef7bac9
gatlingmcgee <rzman03@aol.com>
2023-10-09 18:21:01 -0500

    w05learningactivity

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int num)
+    {
+        _top = num;
+        _bottom = 1;
+    }
+    public Fraction(int numera, int denom)
+    {
+        _top = numera;
+        _bottom = denom;
+    }
+
+
+    public string GetFraction()
+    {
+        string answer = $"{_top} / {_bottom}";
+        return answer;
+    }
+
+    public double GetDecimal()
+    {
+        return (double) _top / (double) _bottom;
+    }
+}
+
